8d5dcd01cbce10cf53e4d9204884d10f7f9b0532
Thandeka93 <tgamede93@gmail.com>
2023-11-29 15:36:26 +0200

    totals

--- services/query.js
+++ services/query.js
@@ -16,6 +16,12 @@ export default function Expense(db) {
     async function allExpenses() {
       return await db.any("SELECT * FROM expense");
     }
+
+    async function categoryTotals() {
+        let totals = await db.any(`
+        SELECT SUM(e.amount), c.category_type FROM category AS c RIGHT JOIN expense AS e ON c.id=e.category_id GROUP BY c.category_type ORDER BY sum DESC;`);
+        return totals;
+      }
     async function deleteExpense(expenseid) {
         await db.none("DELETE FROM expense WHERE id=$1", [expenseid]);
         return allExpenses();
@@ -24,6 +30,7 @@ export default function Expense(db) {
       addExpense,
       expenseForCategory,
       allExpenses,
+      categoryTotals,
       deleteExpense
     };
   }
